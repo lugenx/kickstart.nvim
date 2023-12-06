77be70a00c751de9f70301e4eb0a7b06e8a03829
lugenx <hello@lugenx.com>
2023-12-06 02:03:06 -0500

    add custom init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,47 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+
+local function setupFileBrowser()
+    pcall(require('telescope').load_extension, 'file_browser')
+end
+
+-- Set the shell option - neovim can read .bash_profile file
+vim.opt.shell = "/bin/bash -l"
+
+
+-- Setup an autocommand to run the setup function after Neovim has started
+vim.api.nvim_create_autocmd("VimEnter", {
+    pattern = "*",
+    callback = setupFileBrowser,
+})
+
+-- Set up Prettier autoformat on save for specific file types
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = {"*.js", "*.jsx", "*.ts", "*.tsx"},
+    command = "Prettier",
+})
+
+-- No line numbers in Neovim terminal
+vim.api.nvim_create_autocmd("TermOpen", {
+    pattern = "*",
+    command = "setlocal nonumber norelativenumber"
+  })
+  
+
+return {
+  -- Pretter
+  {
+    'prettier/vim-prettier',
+    run = 'yarn install --frozen-lockfile --production',
+    -- Optionally, you can specify a branch, tag, or commit:
+    -- branch = 'main', -- or specific tag/commit
+  },
+    -- Telescope file_browser
+    {
+    'nvim-telescope/telescope-file-browser.nvim',
+  },
+}
+
+
