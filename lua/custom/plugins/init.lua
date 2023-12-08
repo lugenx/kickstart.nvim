6899be3d601e1181cad53506ce6dc64f72bc1787
lugenx <hello@lugenx.com>
2023-12-07 22:18:30 -0500

    update custom file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,26 +1,65 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
---
+
 -- See the kickstart.nvim README for more information
 
-
-local function setupFileBrowser()
-    pcall(require('telescope').load_extension, 'file_browser')
-end
+-----------------------------------------------------------
+--Settings
+-- Increase distance between line numbers and edge
+vim.wo.numberwidth = 4
 
 -- Set the shell option - neovim can read .bash_profile file
 vim.opt.shell = "/bin/bash -l"
 
+-- Function to setup file browser extension for Telescope
+local function setupFileBrowser()
+    pcall(require('telescope').load_extension, 'file_browser')
+end
 
--- Setup an autocommand to run the setup function after Neovim has started
+-- Function to setup OneDark theme with a darker variant
+local function setupOneDarkDarker()
+    local status_ok, onedark = pcall(require, "onedark")
+    if not status_ok then
+        return
+    end
+
+    onedark.setup({
+        style = 'darker' -- There are different versions like 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer'
+    })
+
+    vim.cmd("colorscheme onedark")
+end
+
+-- Function to set up lualine with a specific theme
+local function setupLualine()
+    local lualine_ok, lualine = pcall(require, "lualine")
+    if not lualine_ok then
+        print("Lualine not loaded")
+        return
+    end
+
+    lualine.setup {
+        options = {
+            icons_enabled = false,
+            theme = 'onedark'
+        }
+    }
+end
+
+-- Combine setup functions to be called when Neovim starts
 vim.api.nvim_create_autocmd("VimEnter", {
     pattern = "*",
-    callback = setupFileBrowser,
+    callback = function()
+        setupFileBrowser()
+        setupOneDarkDarker()
+        setupLualine()
+        vim.cmd("IBLDisable")
+    end,
 })
 
 -- Set up Prettier autoformat on save for specific file types
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = {"*.js", "*.jsx", "*.ts", "*.tsx"},
+    pattern = {"*.js", "*.jsx", "*.ts", "*.tsx", "*.css", "*.html", "*.json"},
     command = "Prettier",
 })
 
@@ -28,21 +67,15 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 vim.api.nvim_create_autocmd("TermOpen", {
     pattern = "*",
     command = "setlocal nonumber norelativenumber"
-  })
-  
+})
 
+-- Return statement for your plugin configurations
 return {
-  -- Pretter
   {
     'prettier/vim-prettier',
     run = 'yarn install --frozen-lockfile --production',
-    -- Optionally, you can specify a branch, tag, or commit:
-    -- branch = 'main', -- or specific tag/commit
   },
-    -- Telescope file_browser
-    {
+  {
     'nvim-telescope/telescope-file-browser.nvim',
   },
 }
-
-
